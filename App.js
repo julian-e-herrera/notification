ab5fafaa2fe123c4ad1485c17750f87eb95217c4
Julian Herrera <julianeherreraok@gmail.com>
2023-02-02 11:49:44 -0300

    ONLY WORKS IN REAL DEVICE

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, StyleSheet, Text, View, Platform } from "react-native";
 import * as Notifications from "expo-notifications";
 import { useEffect } from "react";
 
@@ -14,6 +14,33 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
+  useEffect(() => {
+    async function configPushNotification() {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+      if (!finalStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission required",
+          "Push notification need the appropiate permissions."
+        );
+        return;
+      }
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.DEFAULT,
+        });
+      }
+    }
+    configPushNotification();
+  }, []);
+
   useEffect(() => {
     const subscription = Notifications.addNotificationReceivedListener(
       (notification) => {
@@ -47,10 +74,34 @@ export default function App() {
       trigger: { seconds: 5 },
     });
   };
+
+  async function sendPushNotificationhandler() {
+    const message = {
+      to: "ExponentPushToken[7YPUN1I8FW2xCAWvm0_GYd]", //TODO: should be a variable stored on db
+      sound: "default",
+      title: "Original Title sent from a device!!",
+      body: "And here is the body!",
+      data: { someData: "goes here" },
+    };
+    await fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(message),
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Button title="Schedule Notification" onPress={scheduleNotifHandler} />
+      <Button
+        title="Send Push Notification"
+        onPress={sendPushNotificationhandler}
+      />
       <StatusBar style="auto" />
     </View>
   );
